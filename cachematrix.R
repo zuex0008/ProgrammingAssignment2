7bbc387a5725f4e72c8e65b9ea406c97732bd783
zuex0008 <zuex0008@hotmail.com>
2015-10-25 19:33:16 +0800

    Added the code to calulate inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makecachematrix function will cache the matrix that is passed to the function 
+## the function will return the a list 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+  # initialize inv variable to null
+  inv = NULL
+  
+  # the matrix value is set in the environment
+  set = function(y)
+  {
+    ## the assigned value is passed to the external variable x
+    x <<- y
+    inv <<- NULL
+  }
+  
+  # will return the matrix set above 
+  get = function() x
+  
+  ## set the inverse value to the variable for future use
+  setinv = function(inverse) inv <<- inverse 
+  
+  ## get the inverse value assigned to the variable
+  getinv = function() inv
+  
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cachesolve will find the inverse of the matrix that is passed to the function 
+## from makecachematrix variable, if the inverse is not calculated   
+
+cacheSolve <- function(x, ...)
+{
+  ## @x: output of makeCacheMatrix()
+  ## return: inverse of the original matrix input to makeCacheMatrix()
+  
+  inv = x$getinv()
+  
+  ## if the inverse has already been calculated
+  if (!is.null(inv))
+  {
+    ## get it from the cache and skips the computation. 
+    message("getting cached data")
+    return(inv)
+  }
+  
+  ## get matrix from the makecachematrix get function
+  mat.data = x$get()
+  ## calculate inverese of the matrix 
+  inv = solve(mat.data, ...)
+  
+  ## sets the value of the inverse in the cache via the setinv function.
+  x$setinv(inv)
+  
+  ## return the calculated value 
+  return(inv)
 }
